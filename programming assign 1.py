568a8c43b20958b3f0e40fb110eee4cb69104d88
sourav pratik <105154543+souravpratik@users.noreply.github.com>
2022-05-20 15:15:55 +0530

    Add files via upload

---
+++ programming assign 1.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+##Question 1:
+
+print("Hello Python")
+
+
+# In[2]:
+
+
+##Question 2:
+
+a=int(input("a=enter 1st no."))
+b=int(input("b=enter 2nd no."))
+print(a+b)
+print(a/b)
+
+
+# In[13]:
+
+
+##Question 3:
+
+a=int(input("base length="))
+b=int(input("height of triangle="))
+c=("area=0.5*base length*height ")
+print(c)
+d=0.5*a*b
+print(d)
+
+
+# In[15]:
+
+
+##Question 4:
+
+a=input("a=enter 1st no.")
+b=input("b=enter 2nd no.")
+a,b=b,a
+print("value of a=",a)
+print("value of b=",b)
+
+
+# In[16]:
+
+
+## Question 4:
+
+import random
+print(random.randint(0,9))
+
+
+# In[17]:
+
+
+import random
+print(random.randint(0,9))
+
+
+# SUBMITTED BY- 
+# 
+# SOURAV PRATIK
+# (souravpratik@gmail.com)
